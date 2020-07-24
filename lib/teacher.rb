6617ba98a41d5bebb601a5a2da50c0cb8396f5d7
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-24 21:43:20 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,8 +1,16 @@
 class Teacher < User
+<<<<<<< HEAD
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
     KNOWLEDGE.sample
+=======
+
+  
+
+  def teach
+    KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+>>>>>>> 3ad6586be554aea50f9a21acfdf67ef7ab941819
   end
 end
